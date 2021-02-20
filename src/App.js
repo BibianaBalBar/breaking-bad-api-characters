6d179e0072f33b7601fc4b801762fb22f6246715
BibianaBalBar <balbuenabibi@gmail.com>
2021-02-20 00:53:20 +0000

    characterItem

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from './components/ui/Header';
+import CharacterGrid from './components/characters/CharacterGrid';
 import './App.css';
 
 const App = () => {
@@ -10,7 +11,9 @@ const App = () => {
   useEffect(() => {
     const fetchItems = async () => {
       const result = await axios(`https://www.breakingbadapi.com/api/characters`);
-      console.log(result.data)
+      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
     }
     fetchItems();
   }, [])
@@ -18,6 +21,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
